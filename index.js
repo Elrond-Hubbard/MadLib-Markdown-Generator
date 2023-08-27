7a82e205f03572f130c80e0bfca080ee49c947f4
Elrond-Hubbard <136862666+Elrond-Hubbard@users.noreply.github.com>
2023-08-27 15:56:27 -0400

    wrote simple question template

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    message: 'What is your name?',
+    name: 'name',
+},
+{
+    type: 'input',
+    message: 'What is your quest?',
+    name: 'quest',
+},
+{
+    type: 'list',
+    message: 'What is your favorite color',
+    name: 'color',
+    choices: ['Red', 'Blue', 'Yellow'],
+    default: ['Blue'],
+}];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            console.log(answers);
+        })
+}
 
 // Function call to initialize app
 init();
